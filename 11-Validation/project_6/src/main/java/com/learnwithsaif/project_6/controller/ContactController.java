9042f271f67fbf2119e1b2755ca9a8091f6e3715
saifmalik2004 <saifmalik7827@gmail.com>
2025-03-31 18:38:47 +0530

    project-6

---
+++ 11-Validation/project_6/src/main/java/com/learnwithsaif/project_6/controller/ContactController.java
@@ -0,0 +1,64 @@
+package com.learnwithsaif.project_6.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.learnwithsaif.project_6.model.Contact;
+import com.learnwithsaif.project_6.service.ContactService;
+
+import jakarta.validation.Valid;
+
+
+
+
+@Controller
+public class ContactController {
+
+    private final ContactService contactService;
+
+    @RequestMapping("/contact")
+    public String displayContactPage(Model model) {
+        model.addAttribute("contact",new Contact());
+        return "contact.html";
+    }
+
+    
+    // @PostMapping(value = "/saveMsg")
+    // public ModelAndView saveMessage(
+    //     @RequestParam String name,
+    //     @RequestParam String mobileNum,
+    //     @RequestParam String email,
+    //     @RequestParam String subject,
+    //     @RequestParam String message
+    // ) {
+    //     System.out.println("Name : " + name);
+    //     System.out.println("Mobile Number : " + mobileNum);
+    //     System.out.println("Email : " + email);
+    //     System.out.println("Subject : " + subject);
+    //     System.out.println("Message : " + message);
+
+    //     return new ModelAndView("redirect:/contact");
+    // }
+
+
+
+    @Autowired
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
+ @PostMapping(value="/saveMsg")
+    public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors) {
+        if(errors.hasErrors()){
+            System.out.println("Contact form validation failed due to : " + errors.toString());
+            return "contact.html";
+        }
+        contactService.saveMessageDetail(contact);
+        return "redirect:/contact";
+    }
+
+}
